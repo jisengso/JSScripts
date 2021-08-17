3e032770fd89eae9f2508228c8202c2d31724df2
Jiseng So <jiseng.so@gmail.com>
2021-08-17 08:55:40 +0000

    Add support for odd number of pages.

--- videos/pdfToPng.py
+++ videos/pdfToPng.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python3
-
 """
 Converts a pdf into a series of images with pairs of pages.
 
-Relies on Imagemagick with "convert"
-
-Future capabilities to consider:
-* Consider pdfs with odd numbers of pages
-* Get number of pages before actually processing
+Dependencies:
+        'convert' from ImageMagick
+        'pdfinfo'
 """
+
 import sys
 import subprocess
 import tempfile
+import math
 
 mainCommand = "convert"
-SP = " "
-LB = "["
-RB = "]"
 perInputArgs = "-density 600"
 inputFilename = sys.argv[1]
 outputParameters = "+append +repage -flatten -background white"
 outputPrefix = "image_"
-numDigits = "%03d"
 outputPostfix = ".png"
 pagesPerImage = 2
-noMorePages = "Requested FirstPage is greater than the number of pages in the file"
 
 commandErrors = tempfile.TemporaryFile()
 commandOutput = tempfile.TemporaryFile()
@@ -32,23 +26,41 @@ curPage = 0
 
 errors = b""
 output = b""
+pdfInfo = b""
 
-execString = mainCommand + SP
+subprocess.call(f"pdfinfo {inputFilename}", stdout=commandOutput, shell=True)
+commandOutput.seek(0)
+pdfInfo = commandOutput.read().strip().replace(b"\r", b"")
+pagesLine = ""
 
-while errors == b"":
+for line in pdfInfo.split(b"\n"):
+        if line.startswith(b"Pages:"):
+                pagesLine = line
+                break
+totalPages = int(pagesLine.split()[1])
+numDigits = f"%0{math.ceil(math.log(totalPages, 10))+1}d"
 
-        execString += SP + perInputArgs + SP + inputFilename + LB + str(curPage) + RB + SP
-        if curPage % pagesPerImage == pagesPerImage-1:
-                execString += SP + outputParameters + SP + outputPrefix + numDigits % (curPage/pagesPerImage) + outputPostfix
+print(f"Reading {inputFilename}, which has {totalPages} pages.")
+
+outputtedPages = set()
+
+for curPage in range(totalPages):
+        baseExec = f"{mainCommand} {perInputArgs} {inputFilename}[{curPage}]"
+        outputFilename = f"{outputPrefix}{numDigits % (curPage/pagesPerImage)}{outputPostfix}"
+        execString = ""
+        if curPage % pagesPerImage == 0:
+                execString = f"{baseExec} {perInputArgs} {inputFilename}[{curPage+1}] {outputParameters} {outputFilename}"
+        elif curPage == totalPages-1 and outputFilename not in outputtedPages:
+                execString = f"{baseExec} {outputParameters} {outputFilename}"
+
+        if execString:
                 print (execString)
                 subprocess.call(execString, stdout=commandOutput, stderr=commandErrors, shell=True)
-                execString = mainCommand + SP
                 commandErrors.seek(0)
                 commandOutput.seek(0)
                 errors = commandErrors.read().strip().replace(b"\r", b"")
                 output = commandOutput.read().strip().replace(b"\r", b"")
-        else:
-                pass
-        curPage +=1
+                outputtedPages.add(outputFilename)
+
 print (errors)
 print (output)
